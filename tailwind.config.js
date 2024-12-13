5f56872534b93ea1b29cc62dc2771c3f6ef24665
Marty Umlas <martyumlas@gmail.com>
2024-12-13 17:39:06 +0800

    omni-header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content:  ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    // this will overide all fontsizes
+    // fontSize: {
+    //   '10xl': '10rem'
+    // },
+    extend: {
+      //extending the default tailwind classes
+      fontFamily: {
+        'rubik': ["Rubik"],
+        'pacifico': ['Pacifico']
+      },
+      fontSize: {
+        '10xl': '10rem'
+      },
+      colors: {
+        primary: '#b83975',
+        secondary: {
+          100: '#ecff83'
+        }
+      }
+    },
+    
   },
   plugins: [],
 }
